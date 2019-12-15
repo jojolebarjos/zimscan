a87d3c77e02f8be4451eaa07c688220162abf040
Johan Berdat <jojolebarjos@gmail.com>
2019-12-15 14:46:09 +0100

    Refactor iterator

--- zimscan/reader.py
+++ zimscan/reader.py
@@ -26,49 +26,84 @@ class Reader:
     def __init__(self, file):
         
         # Wrap in buffer, to ensure exact read size
-        self.file = io.BufferedReader(file)
+        self._file = io.BufferedReader(file)
         
         # Read header
         (
-            self.magic,
-            self.major,
-            self.minor,
+            magic,
+            major,
+            minor,
             self.uuid,
-            self.article_count,
-            self.cluster_count,
-            self.url_pointer_list_offset,
-            self.title_pointer_list_offset,
-            self.cluster_pointer_list_offset,
-            self.mime_list_offset,
-            self.main_page,
-            self.layout_page,
-            self.checksum_offset
-        ) = header.unpack(self.file.read(header.size))
+            self._article_count,
+            self._cluster_count,
+            url_pointer_list_offset,
+            title_pointer_list_offset,
+            cluster_pointer_list_offset,
+            mime_list_offset,
+            main_page,
+            layout_page,
+            checksum_offset
+        ) = header.unpack(self._file.read(header.size))
+        
+        # Check format
+        if magic != 72173914:
+            raise IOError('invalid ZIM file')
+        if major != 5 and major != 6:
+            raise IOError(f'ZIM format version {major}.{minor} not supported')
+        
+        # TODO load MIME type list (only if metadata is requested)
+        # TODO load directories, if requested
         
         # Read cluster pointers
-        self.file.seek(self.cluster_pointer_list_offset)
-        self.cluster_pointer_list = numpy.frombuffer(self.file.read(8 * self.cluster_count), uint64)
-    
-    def __len__(self):
-        return self.article_count
-    
-    def __iter__(self):
+        self._file.seek(cluster_pointer_list_offset)
+        self._cluster_pointer_list = numpy.frombuffer(self._file.read(8 * self._cluster_count), uint64)
         
-        # For each cluster...
-        for cluster_index in range(self.cluster_count):
+        # Iterator state
+        self._cluster_index = -1
+        self._cluster_file = None
+        self._blob_count = 0
+        self._blob_index = 0
+        self._blob_offsets = None
+        self._record = None
+    
+    # Number of article is known
+    def __len__(self):
+        return self._article_count
+    
+    # The reader iself is an iterator
+    def __iter__(self):
+        return self
+    
+    # Advance
+    def __next__(self):
+        
+        # Invalidate current record, if any
+        if self._record is not None:
+            if self._record._remaining > 0:
+                self._cluster_file.seek(self._record._remaining, io.SEEK_CUR)
+            self._record._file = None
+            self._record = None
+        self._blob_index += 1
+        
+        # If current cluster is exhausted, advance to next cluster
+        while self._blob_index >= self._blob_count:
+            self._cluster_index += 1
+            if self._cluster_index >= self._cluster_count:
+                raise StopIteration
+            self._blob_index = 0
             
             # Move to cluster
-            self.file.seek(self.cluster_pointer_list[cluster_index])
+            self._file.seek(self._cluster_pointer_list[self._cluster_index])
             
             # Read uncompressed cluster header
-            mode, = numpy.frombuffer(self.file.read(1), uint8)
+            mode, = numpy.frombuffer(self._file.read(1), uint8)
             
             # Wrap input based on compression mode
             compression = mode & 0x0f
             if compression == 4:
-                file = lzma.open(self.file)
+                self._cluster_file = lzma.open(self._file)
             else:
-                file = self.file
+                self._cluster_file = self._file
             
             # Detect blob offset size
             if mode & 0b10000:
@@ -77,32 +112,33 @@ class Reader:
                 offset_type = uint32
             
             # Read first blob offset, used to detect blob count
-            first_blob_offset, = numpy.frombuffer(file.read(offset_type.itemsize), offset_type)
-            blob_count = first_blob_offset // offset_type.itemsize
-            blob_offsets = numpy.empty(blob_count, offset_type)
-            blob_offsets[0] = first_blob_offset
-            blob_offsets[1:] = numpy.frombuffer(file.read(offset_type.itemsize * (blob_count - 1)), offset_type)
+            data = self._cluster_file.read(offset_type.itemsize)
+            first_blob_offset, = numpy.frombuffer(data, offset_type)
+            blob_offset_count = first_blob_offset // offset_type.itemsize
+            self._blob_count = blob_offset_count - 1
             
-            # For each blob...
-            for blob_index in range(blob_count):
-                
-                # Prepare record
-                length = blob_offsets[blob_index + 1] - blob_offsets[blob_index]
-                record = Record(file, length)
-                yield record
-                
-                # Consume remaining bytes
-                if record._remaining > 0:
-                    file.seek(record._remaining, io.SEEK_CUR)
-                
-                # Invalidate record as soon as we continue
-                record._file = None
+            # Read all blob offsets
+            self._blob_offsets = numpy.empty(blob_offset_count, offset_type)
+            self._blob_offsets[0] = first_blob_offset
+            data = self._cluster_file.read(offset_type.itemsize * self._blob_count)
+            self._blob_offsets[1:] = numpy.frombuffer(data, offset_type)
+        
+        # Prepare record
+        length = self._blob_offsets[self._blob_index + 1] - self._blob_offsets[self._blob_index]
+        self._record = Record(self._cluster_file, length)
+        # TODO populate metadata, if requested
+        return self._record
     
+    # Close underlying file
     def close(self):
-        self.file.close()
+        if self._record is not None:
+            self._record._file = None
+        self._file.close()
     
+    # Entering context is a no-op
     def __enter__(self):
         return self
     
+    # Close on context exit
     def __exit__(self, type, value, traceback):
         self.close()
