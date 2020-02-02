68acfec8fabdfcf6f096f10a1300f44e26e78cc8
Johan Berdat <jojolebarjos@gmail.com>
2020-02-02 21:32:15 +0100

    Initial release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 setup(
 
     name = 'zimscan',
-    version = '0.1.1',
+    version = '0.1.0',
     packages = find_packages(),
 
     author = 'Johan Berdat',
@@ -35,8 +35,16 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
     ],
 
     install_requires = [],
